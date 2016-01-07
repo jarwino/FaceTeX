b1947b22a54f2bf16b7084e4a1920a9aaa5f8162
jarwino <jarwino@gmail.com>
2016-01-07 15:49:45 -0500

    fixed for loop errors

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # FaceTeX
 A tool that lets you communicate in LaTeX to your friends over Facebook chat.
-
-## Contributors
-@webfreak7
--- app.js
+++ app.js
@@ -1,107 +1,136 @@
+var http = require('http');
 var config = require('./config.json');
 var fs = require('fs');
 var login = require('facebook-chat-api');
 var mathmode = require('mathmode');
+var mjAPI = require("./node_modules/mathjax-node/lib/mj-single");
 var async = require('async');
-var http = require('http');
-
-
 var results = [];
 var options = {
-    packages: ["amsmath", "amssymb"]
+  packages: ["amsmath", "amssymb"]
 };
+var http = require('http');
+var port = process.env.PORT || 5000;
 
-http.createServer(function (request, response) {
+
+http.createServer(function(req, res) {
+  console.log("ping");
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+  res.end("");
+}).listen(process.env.PORT || 5000);
+
+setInterval(function() {
+  http.get("http://peaceful-tor-3172.herokuapp.com", function(res) {
+    console.log("pong");
+  });
+}, 300000);
 
 
 function isValidLatex(inputString) {
-    return (inputString.slice(0, 1) === '$' &&
-        inputString.slice(-1) === '$' &&
-        inputString !== '$');
+  return (inputString.slice(0, 1) === '$' &&
+    inputString.slice(-1) === '$' &&
+    inputString !== '$');
 }
 
 function extractLatex(inputString) {
-    var length = inputString.length;
-    return inputString.slice(1, length - 1);
+  var length = inputString.length;
+  return inputString.slice(1, length - 1);
 }
 
 function populateResults(inputString) {
-    var resultArray = inputString.split("");
-    var curr;
+  var resultArray = inputString.split("");
 
-    var inMiddle = false;
+  var inMiddle = false;
 
-    for (var i = 0; i < resultArray.length; i++) {
-        if (resultArray[i] === '$') {
-            
-            if (!inMiddle) {
-                curr = "";
-            } else {
-                results.push(curr);
-            }
+  for (var i = 0; i < resultArray.length; i++) {
+    if (resultArray[i] === '$') {
 
-            inMiddle = !inMiddle; //flip switch
-
-        } else {
-            if (inMiddle) {
-                curr += resultArray[i];
-            }
-        }
-
+      if (!inMiddle) {
+        results.push("");
+      }
+      inMiddle = !inMiddle;
+    } else {
+      if (inMiddle) {
+        results[results.length - 1] += resultArray[i];
+      }
     }
+
+  }
 
 }
 
 login({
-    email: config.username,
-    password: config.password
+  email: config.username,
+  password: config.password
 }, function callback(err, api) {
+  if (err) return console.error(err);
+  api.setOptions({
+    listenEvents: true
+  });
+  var interpret = api.listen(function(err, event) {
     if (err) return console.error(err);
-    api.setOptions({
-        listenEvents: true
-    });
-    var interpret = api.listen(function(err, event) {
-        if (err) return console.error(err);
 
-        switch (event.type) {
-            case "message":
-                //console.log(isValidLatex(event.body));
-                populateResults(event.body);
+    switch (event.type) {
+      case "message":
+        //console.log(isValidLatex(event.body));
+        populateResults(event.body);
 
 
-                console.log(results);
+        console.log(results);
 
-                for (var i = 0; i < results.length; i++) {
+        mjAPI.start();
 
-                    var outputFile = fs.createWriteStream('output.png');
+        mjAPI.config({
+            MathJax: {
+              SVG: {
+                font: "TeX"
+              }
+            },
+            extensions: ""
+          });
 
-                    outputFile.on('finish', function() {
-                        var msg = {
-                            attachment: fs.createReadStream('output.png')
-                        };
-                        api.sendMessage(msg, event.threadID, function(err, messageInfo) {
+        for (var i = 0; i < results.length; i++) {
 
-                        });
-                    });
+          mjAPI.typeset({
+            math: results[i],
+            format: "inline-TeX", // "inline-TeX", "MathML"
+            png: true, //  svg:true,
+            dpi: 800,
+            ex: 50,
+            width: 100
+          }, function(data) {
+            console.log(data);
+            if (!data.errors) {
 
-                    mathmode(results[i], options).pipe(outputFile);
+              var base64Data = data.png.replace(/^data:image\/png;base64,/, "");
 
+              fs.writeFile('file' + i + '.png', base64Data, 'base64', function(err) {
+                if (err) {
+                  throw err;
+                } else {
+                  var msg = {
+                    attachment: fs.createReadStream('file' + i + '.png')
+                  };
+                  console.log('hey');
+                  api.sendMessage(msg, event.threadID);
                 }
 
-                results = [];
+              });
+            }
+          });
 
-                api.markAsRead(event.threadID, function(err) {
-                    if (err) console.log(err);
-                });
-                //api.sendMessage("TEST BOT: " + event.body, event.threadID);
-                break;
-            case "event":
+        } // end of for loop
 
-                break;
-        }
-    });
+        results = [];
+
+        api.markAsRead(event.threadID, function(err) {
+          if (err) console.log(err);
+        });
+        break;
+      case "event":
+        break;
+    }
+  });
 });
-
-}).listen(process.env.PORT || 5000);
-
- console.log('Server running at http://127.0.0.1:5000/');
